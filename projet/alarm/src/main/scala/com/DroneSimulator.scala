5a5b6a9d7e99880421f25a8fa78522641bbfd09c
romane <rthoirey@hotmail.fr>
2020-03-05 17:55:00 +0100

    first commit

---
+++ projet/alarm/src/main/scala/com/DroneSimulator.scala
@@ -0,0 +1,46 @@
+package com
+
+import scala.io.Source
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata, Callback}
+
+object DroneSimulator {
+
+  import java.util.Properties
+
+  val TOPIC = "drone"
+
+  val  props = new Properties()
+
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+
+
+  def main(args: Array[String]) : Unit = {
+
+    if (args.length < 2) {
+      println("Usage: ./csv_parser file server [server2 ...]")
+    } else {
+      val fileName = args.head
+      if (new java.io.File(fileName).isFile) {
+        val servers = args.drop(1).map(_ + ":9092").mkString(",")
+        println(servers)
+        props.put("bootstrap.servers", servers)
+
+        val producer = new KafkaProducer[String, String](props)
+        val source = Source.fromFile(fileName)
+        source.getLines().foreach((line: String) =>
+          producer.send(new ProducerRecord(TOPIC, "key", line))
+        )
+        source.close()
+        producer.close()
+
+      } else {
+        println("File not found!")
+      }
+
+    }
+
+  }
+
+}
