c6ad7f0e447e8b18d66ea29478c6d914dda45d93
romane <rthoirey@hotmail.fr>
2020-03-10 20:44:55 +0100

    scala consumer

--- projet/alarm/src/main/scala/com/Alarm.scala
+++ projet/alarm/src/main/scala/com/Alarm.scala
@@ -1,45 +1,47 @@
 package com
 
 import scala.io.Source
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata, Callback}
+import org.apache.kafka.clients.consumer.{KafkaConsumer, ConsumerRecord, ConsumerConfig}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
 
 object Alarm {
 
   import java.util.Properties
+  import java.util
 
-  val TOPIC = "alarm"
+  val props = new Properties()
 
-  val  props = new Properties()
-
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
 
 
   def main(args: Array[String]) : Unit = {
 
-    if (args.length < 2) {
-      println("Usage: ./csv_parser file server [server2 ...]")
-    } else {
-      val fileName = args.head
-      if (new java.io.File(fileName).isFile) {
-        val servers = args.drop(1).map(_ + ":9092").mkString(",")
-        println(servers)
-        props.put("bootstrap.servers", servers)
-
-        val producer = new KafkaProducer[String, String](props)
-        val source = Source.fromFile(fileName)
-        source.getLines().foreach((line: String) =>
-          producer.send(new ProducerRecord(TOPIC, "key", line))
-        )
-        source.close()
-        producer.close()
-
-      } else {
-        println("File not found!")
-      }
-
+    println(args.length)
+    if (args.length < 4) {
+      System.err.println("Usage: KafkaWordCount <zkQuorum><group> <topics> <numThreads>")
+      System.exit(1)
     }
+
+    val Array(zkQuorum, group, topics, numThreads) = args
+    println("ici")
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    ssc.checkpoint("checkpoint")
+
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L))
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+    spark-submit --packages org.apache.spark:spark-streaming -kafka_2.10:1.6.0 --class "KafkaWordCount" --master local[4] target/scala-2.10/spark -kafka-project_2.10-1.0.jar localhost:2181 <group name> <topic name> <number of threads>
 
   }
 
