a59b02c4492fba81d2917af95228d7446c4804c1
romane <rthoirey@hotmail.fr>
2020-03-17 17:50:18 +0100

    connextion to producer

--- projet/alarm/build.sbt
+++ projet/alarm/build.sbt
@@ -1,17 +1,12 @@
-name := "csv-parser"
+name := "spark-playground"
 
-version := "0.1"
-
+version := "1.0"
 
 scalaVersion := "2.11.8"
 
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6"
-libraryDependencies += "org.apache.kafka" % "kafka-streams" % "2.0.1"
-libraryDependencies += "org.apache.kafka" %% "kafka-streams-scala" % "2.0.1"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.0.0"
-libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
 
-libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.8.0-alpha2"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.1.0"
 
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.1.0"
--- projet/alarm/src/main/scala/com/Alarm.scala
+++ projet/alarm/src/main/scala/com/Alarm.scala
@@ -1,66 +1,34 @@
-package com
-
-import scala.io.Source
-import org.apache.kafka.clients.consumer.{KafkaConsumer, ConsumerRecord, ConsumerConfig}
-import org.apache.spark.SparkConf
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark._
 import org.apache.spark.streaming._
-
+import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.kafka010._
-
-object Alarm {
-
-  import java.util.Properties
-  import java.util
 
 
-  def main(args: Array[String]) : Unit = {
+object Alarm extends App {
 
-    val sparkConf = new SparkConf()
-      .setAppName("Alarm")
-      .setMaster("spark://localhost:9092")
+  println("Program Started")
 
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
-    ssc.checkpoint("checkpoint")
+  val conf = new SparkConf().setMaster("local[2]").setAppName("Alarm")
 
-    val TOPIC = Array("test")
-
-    val kafkaParams = Map[String, Object](
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
-      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG ->
-        "org.apache.kafka.common.serialization.StringDeserializer",
-      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG ->
-        "org.apache.kafka.common.serialization.StringDeserializer",
-      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest",
-      ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> "false"
-    )
-
-    val consumerStrategy = Subscribe[String,String](TOPIC, kafkaParams)
-    val stream = KafkaUtils.createDirectStream[String, String](
-      ssc,
-      PreferConsistent,
-      consumerStrategy)
-
-    val lines = stream.map(_.value())
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
-
-    ssc.start()
-    ssc.awaitTermination()
-
-    // spark-submit --packages org.apache.spark:spark-streaming -kafka_2.10:1.6.0 --class "KafkaWordCount" --master local[4] target/scala-2.10/spark -kafka-project_2.10-1.0.jar localhost:2181 <group name> <topic name> <number of threads>
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "spark-playground-group",
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
+  val ssc = new StreamingContext(conf, Seconds(10))
 
 
-    //./bin/spark-submit \
-    //  --class <main-class> \
-    //  --master <master-url> \
-    //  --deploy-mode <deploy-mode> \
-    //  --conf <key>=<value> \
-    //  ... # other options
-    //  <application-jar> \
-    //  [application-arguments]
-  }
+  val inputStream = KafkaUtils.createDirectStream(ssc, PreferConsistent, Subscribe[String, String](Array("drone"), kafkaParams))
+  val processedStream = inputStream
+    .flatMap(record => record.value.split(","))
+
+  processedStream.print()
+  ssc.start()
+  ssc.awaitTermination()
 
 }
